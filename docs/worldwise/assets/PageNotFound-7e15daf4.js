d151bf195f303b4d8b0b0026d3c2c0ee6630d604
Mostafa Chiba <chibamostafa.real@gmail.com>
2025-02-05 20:13:18 +0100

    9/13 projects completed

--- docs/worldwise/assets/PageNotFound-7e15daf4.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./index-94f09eb2.js";function e(){return n("div",{children:n("h1",{children:"Page not found 😢"})})}export{e as default};
---
+++ docs/worldwise/worldwise/assets/PageNotFound-858d216f.js
@@ -0,0 +1,1 @@
+import{a as n}from"./index-4fa6beb8.js";function e(){return n("div",{children:n("h1",{children:"Page not found 😢"})})}export{e as default};
